7fa989ea49cb2b05227375d153b5305824e24697
Olcay Arpac <olcayarpac@yahoo.com>
2020-06-10 01:06:25 +0300

    commit

--- src/messageComponent.js
+++ src/messageComponent.js
@@ -7,13 +7,16 @@ import SendIcon from '@material-ui/icons/Send';
 class messageComponent extends React.Component {
     render() {
         return <div className="messageComponent">
+            <div className="messages">
+                   
+            </div>
             <Grid container className="messageGrid" alignItems="flex-end" justify="flex-start">
                 <Grid Item xs={11}>
-                    <TextField className="messageInput" id="input-with-icon-grid" label="Message" color="secondary" />
-                 </Grid>
-                <Grid Item xs={1}><SendIcon color="secondary"/></Grid>
+                <TextField className="messageInput" id="input-with-icon-grid" label="Message" color="secondary" />
             </Grid>
-        </div>
+            <Grid Item xs={1}><SendIcon color="secondary" /></Grid>
+            </Grid>
+        </div >
     }
 }
 
